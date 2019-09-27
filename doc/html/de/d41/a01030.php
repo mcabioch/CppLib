b36f984b8025c7430c2591d544c1ea26dd4b2246
Mathias Cabioch-Delalande <mathias.cabiochdelalande@gmail.com>
2019-09-27 01:34:41 -0400

    Mise à jour sur raspberry

--- doc/html/de/d41/a01030.php
+++ doc/html/de/d41/a01030.php
@@ -39,7 +39,7 @@
 				</table>
 			</div>
 			<?php
-				$text = "Generated on Fri Sep 27 2019 01:33:06 for C++ by";
+				$text = "Generated on Fri Sep 27 2019 01:33:44 for C++ by";
 				$text = substr($text, 0, sizeof($text) - 4);
 				$tmp = substr($text, 17);
 				$tmp = substr($tmp, 0, strpos($tmp, " for"));
